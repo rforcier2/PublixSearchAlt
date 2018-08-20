d02d297aeebfcc911352012749ad2118a1393ee1
Ron Forcier II <rforcier2@gmail.com>
2018-08-20 14:35:09 -0400

    Update index.cs

--- index.cs
+++ index.cs
@@ -1,13 +1,8 @@
-//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Rextester
-{
     public class Program
     {
         public static void Main(string[] args)
@@ -21,4 +16,3 @@ namespace Rextester
             }
         }
     }
-}
